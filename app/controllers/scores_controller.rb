9beec59f6be206e913547490852cb4584e1fa61f
asaf schers <schers@riskified.com>
2017-12-02 20:31:14 +0200

    Score by titanic hard coded features + start form

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,9 +1,31 @@
 class ScoresController < ActionController::Base
 
   def index
+    random_forest = Scoruby.load_model 'app/pmmls/titanic_rf.pmml'
+    @features =  {
+      Sex: 'male',
+      Parch: 0,
+      Age: 30,
+      Fare: 9.6875,
+      Pclass: 2,
+      SibSp: 0,
+      Embarked: 'Q'
+    }
+    
+    @score = random_forest.predict(@features)
+
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: {} }
+      format.json { render json: { score: @score, features: @features } }
+    end
+  end
+
+  def score
+    puts 1
+    
+    respond_to do |format|
+      format.html # score.html.erb
+      format.json { render json: {  } }
     end
   end
 end
