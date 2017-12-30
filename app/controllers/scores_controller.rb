c7a4125029de4392676099f92abfcf71b3dc7313
asaf schers <schers@riskified.com>
2017-12-31 00:23:23 +0200

    Continuous feature names from model

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,6 +1,7 @@
 class ScoresController < ActionController::Base
 
   def index
+    random_forest = Scoruby.load_model 'app/pmmls/titanic_rf.pmml'
     @features =  {
       Sex: 'male',
       Parch: 1,
@@ -10,20 +11,23 @@ class ScoresController < ActionController::Base
       SibSp: 1,
       Embarked: 'Q'
     }
+    @continuous_features = random_forest.continuous_features
   end
 
   def score
     random_forest = Scoruby.load_model 'app/pmmls/titanic_rf.pmml'
 
-    @features =  {
+    @features = {
       Sex: params['sex'],
-      Parch: params['parch'].to_f,
-      Age: params['age'].to_f,
-      Fare: params['fare'].to_f,
-      Pclass: params['pclass'].to_f,
-      SibSp: params['sibsp'].to_f,
+      Parch: params['Parch'].to_f,
+      Age: params['Age'].to_f,
+      Fare: params['Fare'].to_f,
+      Pclass: params['Pclass'].to_f,
+      SibSp: params['SibSp'].to_f,
       Embarked: params['embarked']
     }
+
+    @continuous_features = random_forest.continuous_features
 
     begin
       @score = random_forest.predict(@features)
