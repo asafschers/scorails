cc07978df4070f06986600a877d0289fe53f5e09
asaf schers <schers@riskified.com>
2018-01-13 13:09:12 +0200

    Read categorical features and categories from model

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -2,27 +2,26 @@ class ScoresController < ActionController::Base
 
   def index
     random_forest = Scoruby.load_model 'app/pmmls/titanic_rf.pmml'
-    @features =  {
-      Sex: 'male',
-      Embarked: 'Q'
-    }
+
+    @categorical_options = random_forest.categorical_features
+    @categorical_values = @categorical_options.transform_values{|v| v.first}
     @continuous_features = (Hash[random_forest.continuous_features.product([3])])
   end
 
   def score
     random_forest = Scoruby.load_model 'app/pmmls/titanic_rf.pmml'
 
-    @features = {
-      Sex: params['sex'],
-      Embarked: params['embarked']
-    }
-
     @continuous_features = random_forest.continuous_features.each_with_object({}) do |feature, hash|
       hash[feature] = params[feature].to_f
     end
 
+    @categorical_options = random_forest.categorical_features
+    @categorical_values = random_forest.categorical_features.keys.each_with_object({}) do |feature, hash|
+      hash[feature] = params[feature]
+    end
+
     begin
-      @score = random_forest.predict(@features.merge(@continuous_features))
+      @score = random_forest.predict(@continuous_features.merge(@categorical_values))
     rescue => e
       @score = e
     end
