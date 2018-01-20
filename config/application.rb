009e8b4d00fe1331fca7d26b2211aa46ae508229
asaf schers <schers@riskified.com>
2018-01-20 02:42:58 +0200

    autoload lib

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,7 @@ module Scorails
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
   end
 end
