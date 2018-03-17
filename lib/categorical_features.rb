23d40f58762386c3c1a3809d268016052c337534
asaf schers <schers@riskified.com>
2018-03-17 15:56:08 +0200

    Refactor CategoricalFeatures - remove methods

--- lib/categorical_features.rb
+++ lib/categorical_features.rb
@@ -5,26 +5,17 @@ class CategoricalFeatures
   end
 
   def view_data
-    @params.nil? ? default_categorical_features : input_categorical_features
+    @categorical_features.keys.each_with_object({}) do |feature, hash|
+      hash[feature] = {
+        list: @categorical_features[feature],
+        selected: @params[feature]
+      }
+    end
   end
 
   def score_data
     @categorical_features.keys.each_with_object({}) do |feature, hash|
       hash[feature] = @params[feature]
-    end
-  end
-
-  private
-
-  def input_categorical_features
-    @categorical_features.keys.each_with_object({}) do |feature, hash|
-      hash[feature] = { list: @categorical_features[feature], selected: @params[feature] }
-    end
-  end
-
-  def default_categorical_features
-    @categorical_features.keys.each_with_object({}) do |feature, hash|
-      hash[feature] = { list: @categorical_features[feature], selected: nil }
     end
   end
 end
